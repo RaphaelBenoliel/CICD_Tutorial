bf0c8acb7da6a0d853e240a3bd5df87885cec1a5
Raphael Benoliel <raphael2gb@gmail.com>
2023-03-23 11:16:15 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('A new request has arrived to index.js');
+    res.send('<h1 style="color: blue">Hi from the server main page!!</h1>');
+});
+
+app.get('/apple', (req, res) => {
+    console.log('A new apple request has arrived to index.js');
+    res.send('<h1>Hello apple page</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up and running at port: ${port}`);
+});
---
+++ tests/Arrays.spec.js
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+
+describe('Arrays', () => {
+    describe ("#sort", ()=> {
+        if ('Sorting names array', () =>{
+            var names = ['Dany', 'Moshe', 'Adam'];
+            expect(names.sort()).to.be.eql(['Adam', 'Dany' ,'Moshe']);
+        })
+    });
+
+})
