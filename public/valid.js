fcc0b55fa68c03e0f8e4d8ca170630b6bca3ac19
Raphael Benoliel <raphael2gb@gmail.com>
2023-03-25 01:33:12 +0300

    add login page

--- public/valid.js
+++ public/valid.js
@@ -1,7 +1,7 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-use-before-define */
 /* eslint-disable camelcase */
-/* eslint-disable no-undef */
+/* eslint-disable no-undef 
 // Validtion Code For Inputs
 
 const { email } = document.forms.form;
@@ -40,3 +40,49 @@ function pass_Verify() {
         return true;
     }
 }
+*/
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+email.addEventListener('input', email_Verify);
+password.addEventListener('input', pass_Verify);
+
+const emailBox = document.querySelector('.emailBox');
+const emailText = document.querySelector('.emailText');
+const emailPattern = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/;
+function validated() {
+    if(!(email.value.match(emailPattern))){
+        emailBox.classList.add('invalid');
+        emailBox.classList.remove('valid');
+        emailText.innerHTML = "Must be a valid email address.";
+        return false; 
+    }
+    if (!(password.value.match(passPattern))) {
+        passBox.classList.add('invalid');
+        passBox.classList.remove('valid');
+        passText.innerHTML = "Your password must be at least 6 characters as well as contain at least one uppercase, one lowercase, and one number.";
+        return false; 
+    }
+}
+function email_Verify(){
+    if(email.value.match(emailPattern)){
+        emailBox.classList.add('valid');
+        emailBox.classList.remove('invalid');
+        emailText.innerHTML = "Your Email Address in Valid";
+        return true; 
+    }
+  
+}
+const passBox = document.querySelector('.passBox');
+const passText = document.querySelector('.passText');
+const passPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
+function pass_Verify(){
+    if(password.value.match(passPattern)){
+	    passBox.classList.add('valid');
+		passBox.classList.remove('invalid');
+		passText.innerHTML = "Your Password in Valid"; 
+        return true;
+	}    
+}
+
+
