6fe1f31c175d9c71a82a1e43c3a0cefe84124676
Raphael Benoliel <raphael2gb@gmail.com>
2023-03-24 13:17:15 +0300

    add msg

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     console.log('A new request has arrived to index.js');
-    res.send('<h1 style="color: blue">Hi from the server main page!!</h1>');
+    res.send('<h1 style="color: blue">Hi from the server main page!?!</h1>');
 });
 
 app.get('/apple', (req, res) => {
