881c5b5c9221609f6c35f2236309b3efcdac354b
Raphael Benoliel <raphael2gb@gmail.com>
2023-03-24 23:35:05 +0300

    add login page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('A new request has arrived to index.js');
-    res.send('<h1 style="color: blue">Hi from the server main page!?!</h1>');
-});
+app.use(express.static('public'));
+app.use('/css', express.static(`${__dirname}/public/css`));
+app.use('/js', express.static(`${__dirname}/public/js`));
+app.use('/jpg', express.static(`${__dirname}/public/jpg`));
 
-app.get('/apple', (req, res) => {
-    console.log('A new apple request has arrived to index.js');
-    res.sendFile('page.html', { root: __dirname });
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    console.log('A new request has arrived to index');
+    res.render('page');
+});
+app.post('/home', (req, res) => {
+    console.log('A new user has login to page');
+    res.render('home');
 });
 
 app.listen(port, () => {
